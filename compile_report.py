6a5f49efb639cd298db3fbc0bc3e1f645593eb33
Guy Romm <guyromm@gmail.com>
2011-06-17 08:05:28 +0300

    some report enhancements + huge bugfix

--- compile_report.py
+++ compile_report.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python
 
 import json,glob,os,datetime,re,sys
-import dateutil.parser
 from commands import getstatusoutput as gso
 usermap = {re.compile('milez'):'guyromm@gmail.com'
            ,re.compile(re.escape('3demax@ukr.net')):'3demax@gmail.com'
            ,re.compile(re.escape('dima@dima-Latitude-D820.')):'maltsev.dima7@gmail.com'
+           ,re.compile(re.escape('nskrypnik@hosted-by.leaseweb.com')):'nskrypnik@gmail.com'
+           ,re.compile(re.escape('Den@.(none)')):'mr.dantsev@gmail.com'
            }
 GITHUB_USER = open('githubuser.txt','r').read().strip()
 GITHUB_PASSWORD = open('githubpw.txt','r').read().strip()
@@ -17,18 +18,42 @@ by_project={}
 by_date={}
 
 user_project={}
+project_user={}
 user_date={}
 user_project_date={}
 project_date={}
 
 for proj in GITHUB_PROJECTS:
     ofn = os.path.join('data','%s.json'%proj)
-    if not os.path.exists(ofn):
+    fetchnew=True
+
+    if os.path.exists(ofn):
+        st = os.stat(ofn)
+        fmt = datetime.datetime.fromtimestamp(st.st_mtime)
+        oneh = (datetime.datetime.now()-datetime.timedelta(days=1))
+        #print 'fmt = %s; oneh = %s'%(fmt,oneh)
+        if fmt>=oneh:
+            fetchnew=False
+        else:
+            os.unlink(ofn)
+    if fetchnew:
         print 'fetching project %s'%proj
-        cmd = "curl -u '%s' 'http://github.com/api/v2/json/commits/list/%s/%s/master' > data/%s.json"%(GITHUB_PASSWORD,GITHUB_USER,proj,proj)
-        st,op=gso(cmd) ; assert st==0
+        pagenum=1
+        wr={'commits':[]}
+        while True:
+            print 'taking page %s'%pagenum
+            cmd = "curl -s -u '%s' 'http://github.com/api/v2/json/commits/list/%s/%s/master?page=%s'"%(GITHUB_PASSWORD,GITHUB_USER,proj,pagenum)
+            st,op=gso(cmd) ; assert st==0
+            pagenum+=1
+            dt = json.loads(op)
+            if 'commits' in dt: print '%s entries'%len(dt['commits'])
+            if 'commits' not in dt or not len(dt['commits']): break
+            wr['commits']+=dt['commits']
+
+        fp = open(ofn,'w') ; fp.write(json.dumps(wr)); fp.close()
+            
     else:
-        print 'already got %s'%proj
+        print '%s is recent enough'%proj
 
 def initarr():
     return {'times':0,'diff':0,'removed':0,'added':0,'ids':[]}
@@ -60,11 +85,11 @@ for fn in glob.glob('data/*.json'):
 
         if fr and date.date()<fr: continue
         if to and date.date()>to: continue
-        print '%s -> %s on %s'%(user,proj,date)
+        #print '%s -> %s on %s'%(user,proj,date)
         comid = '/%s/%s/commit/%s'%(GITHUB_USER,projfn,c['id'])
         commsg = c['message']
         if not os.path.exists(comfn):
-            print 'fetching commit'
+            print 'fetching commit %s'%c['id']
             comurl = 'http://github.com/api/v2/json/commits/show/%s/%s/%s'%(GITHUB_USER,projfn,c['id'])
             curlcmd = 'curl -u %s %s > %s'%(GITHUB_PASSWORD,comurl,comfn)
             st,op = gso(curlcmd) ; assert st==0
@@ -92,6 +117,8 @@ for fn in glob.glob('data/*.json'):
             added = 0
 
         if proj not in project_date: project_date[proj]={}
+        if proj not in project_user: project_user[proj]={}
+        if user not in project_user[proj]: project_user[proj][user]=initarr()
         if date.date() not in project_date[proj]: project_date[proj][date.date()] = initarr()
         
         if user not in by_user: 
@@ -110,7 +137,7 @@ for fn in glob.glob('data/*.json'):
             o['ids'].append([comid,commsg,user,date])
             o['ids'].sort(idsort)
         incr(by_user[user])
-
+        incr(project_user[proj][user])
         if proj not in by_project: 
             by_project[proj]=initarr()
 
@@ -148,7 +175,7 @@ dtendpat = "</tbody></table>"
 rowpat = "<tr><td>%s</td><td>%s</td><td>%s</td><td>%s</td><td>%s</td><td>%s</td></tr>\n"
 def mkrow(date,dt,commits=True):
     if commits:
-        cm = ', '.join(["<a href='https://github.com/%s' title='%s by %s on %s'>%s</a>"%(com,commsg,user,stamp,com.split('/')[4][0:4]) for com,commsg,user,stamp in dt['ids']])
+        cm = ', '.join(["<a href='https://github.com%s' title='%s by %s on %s'>%s</a>"%(com,commsg,user,stamp,com.split('/')[4][0:4]) for com,commsg,user,stamp in dt['ids']])
     else:
         cm=''
     rt= rowpat%(date,dt['times'],dt['added'],dt['removed'],dt['diff'],cm)
@@ -159,8 +186,12 @@ def srt2(i1,i2):
     return cmp(i1[1]['diff'],i2[1]['diff'])
 def srt3(i1,i2):
     return cmp(i1[0],i2[0])
-if fr:op+="<small>commits are starting from %s</small><br />"%(fr)
-if to:op+="<small>commits are until %s</small></br >"%(to)
+opa=[]
+if fr:opa.append("commits are starting from %s"%(fr))
+if to:opa.append("commits are until %s"%(to))
+opa.append("generated on %s"%datetime.datetime.now())
+op+=' :: '.join(['<small>%s</small>'%ope for ope in opa])+'<br />'
+
 op+="<h1>user totals</h1>"
 op+=dtpat
 uitems = by_user.items()
@@ -168,7 +199,7 @@ uitems.sort(srt2,reverse=True)
 for user,commits in uitems:
     op+=mkrow(user,commits,commits=False)
 op+=dtendpat
-op+="<h1>project totals</h1>"
+op+="<h1>project totals by date</h1>"
 for proj,dates in project_date.items():
     op+="<h2>%s</h2>"%proj
     op+=dtpat
@@ -177,7 +208,16 @@ for proj,dates in project_date.items():
     for date,commit in commits:
         op+=mkrow(date,commit,commits=True)
     op+=dtendpat
+op+="<h1>project totals by commiter</h1>"
+for proj,commiters in  project_user.items():
+    op+="<h2>%s</h2>"%proj
+    op+=dtpat.replace('date','commiter')
+    commits = commiters.items()
+    commits.sort(srt2,reverse=True)
+    for user,commit in commits:
+        op+=mkrow(user,commit,commits=True)
 
+    op+=dtendpat
 for user,projects in user_project_date.items():
     op+="<h1>%s commits by %s into %s project(s)</h1>\n"%(by_user[user]['times'],user,len(user_project[user]))
 
